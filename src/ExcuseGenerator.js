277c09016839b430a3d12f721817b96952fe5501
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2023-11-06 17:27:13 +0000

    First commit

---
+++ src/ExcuseGenerator.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "driver", "dog", "comedian", "pinecone"];
+  let action = ["took my", "stole my", "hit my", "juggled my", "wet my"];
+  let possesion = ["phone", "shoe", "my nail", "laptop", "underwear"];
+  let where = [
+    "in the gutter",
+    "on my balcony",
+    "in my driveway",
+    "in my kitchen",
+  ];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possesion[possesionIndex] +
+    " " +
+    where[whereIndex]
+  );
+};
